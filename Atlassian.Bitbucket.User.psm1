d8a5fd7d2219a7cd6c0f787876423d797623dd1f
Chris Holmes <chris.holmes@ephesoft.com>
2020-02-11 14:16:43 -0700

    added Get-BitbucketUsersByGroup cmdlet

--- Atlassian.Bitbucket.User.psm1
+++ Atlassian.Bitbucket.User.psm1
@@ -14,3 +14,19 @@ function Get-BitbucketUser {
         return Invoke-BitbucketAPI -Path $endpoint -Paginated
     }
 }
+
+function Get-BitbucketUsersByGroup {
+    [CmdletBinding()]
+    param(
+        [Parameter( ValueFromPipelineByPropertyName=$true,
+                    HelpMessage='Name of the team in Bitbucket.  Defaults to selected team if not provided.')]
+        [string]$Team = (Get-BitbucketSelectedTeam),
+        [Parameter (HelpMessage='The group slug')]
+        [string]$GroupSlug
+    )
+
+    Process {
+        $endpoint = "groups/$Team/$GroupSlug/members"
+        return Invoke-BitbucketAPI -Path $endpoint -API_Version '1.0'
+    }
+}
