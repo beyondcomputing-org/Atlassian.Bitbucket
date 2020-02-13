fc56e771bed9dceada6923a35e9422116dda9595
Chris Holmes <chris.holmes@ephesoft.com>
2020-02-13 10:19:28 -0700

    added help to public facing functions

--- Atlassian.Bitbucket.User.psm1
+++ Atlassian.Bitbucket.User.psm1
@@ -1,5 +1,17 @@
 using module .\Atlassian.Bitbucket.Authentication.psm1
-
+<#
+.Synopsis
+   Gets the list of users with access to at least one repository for the team specified.
+.DESCRIPTION
+   This function returns a list of all the users with access to at least one repository given the team specified. If no team is specified,
+   defaults to the selected team.
+.EXAMPLE
+   Get-BitbucketUser
+   Returns a list of users of the default team.
+.EXAMPLE
+   Get-BitbucketUser -Team $Team
+   Returns a list of users of the specified team.
+#>
 function Get-BitbucketUser {
     [CmdletBinding()]
     param(
@@ -10,11 +22,22 @@ function Get-BitbucketUser {
 
     Process {
         $endpoint = "users/$Team/members"
-
         return Invoke-BitbucketAPI -Path $endpoint -Paginated
     }
 }
-
+<#
+.Synopsis
+   Gets the list of users associated to the group slug specified.
+.DESCRIPTION
+   This function returns a list of all the users associated to the group slug specified. If no team is specified,
+   defaults to the selected team.
+.EXAMPLE
+   Get-BitbucketUsersByGroup -GroupSlug $GroupSlug
+   Returns a list of users associated to the specified group slug for the default team.
+.EXAMPLE
+   Get-BitbucketUsersByGroup -Team $Team -GroupSlug $GroupSlug
+   Returns a list of users associated to the specified group slug for the specified team.
+#>
 function Get-BitbucketUsersByGroup {
     [CmdletBinding()]
     param(
