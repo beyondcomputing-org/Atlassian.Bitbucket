6ad8cc570b7b2a5eb0d301a0ce59c89f2e33dc79
stitus-ephesoft <shaun.titus@ephesoft.com>
2021-05-03 08:44:44 -0600

    Streamline logic changes

--- Atlassian.Bitbucket.Authentication.psm1
+++ Atlassian.Bitbucket.Authentication.psm1
@@ -126,18 +126,19 @@ function Invoke-BitbucketAPI {
         # Process Pagination
         do
         {
-            # Avoid any sort of redirect to a separate hostname or endpoint and only follow the new query parameters for pagination
-            $counter++
-            $_endpoint = "$($baseUrl)?$(($_endpoint.split('?'))[1])"
             Write-Debug "URI: $_endpoint"
             $return = Invoke-RestMethod -Uri $_endpoint -Method $Method -Body $Body -Headers $Auth.GetAuthHeader() -ContentType $ContentType
+            $_endpoint = $return.next
+
+            # Avoid any sort of redirect to a separate hostname or endpoint and only follow the new query parameters for pagination
+            $queryParts = $_endpoint.split('?')
+            $queryString = $queryParts[1..$($queryParts.count)] -join('')
+            $_endpoint = "$($baseURL)?$queryString"
 
             # Workaround bug BCLOUD-20796 (https://jira.atlassian.com/browse/BCLOUD-20796) - Incorrect URL in next property on /repositories/{workspace}/{repo_slug}/deployments/ endpoint
+            $counter++
             If ($baseURL -match '.*\/repositories\/.*\/.*\/deployments\/') {
                 $_endpoint = $_endpoint -replace ("page=$counter", "page=$($counter+1)")
-            }
-            Else {
-                $_endpoint = $return.next
             }
         }
         while ($return.next)
